cb1770ff7e0c9276fdb80c64c4f17b06b47da774
Aki <kieran.mortimer@gmail.com>
2020-05-26 15:22:00 +0200

    atm js

--- Intermediate Exercise 1/js/main.js
+++ Intermediate Exercise 1/js/main.js
@@ -1,23 +1,23 @@
 let studentGrades = [
-    ["Martin", 76],
-    ["Thomas", 85],
-    ["Klaus", 65],
-    ["Maria", 93],
-    ["David", 81],
+	["Martin", 76],
+	["Thomas", 85],
+	["Klaus", 65],
+	["Maria", 93],
+	["David", 81],
 ];
 
 for (let student of studentGrades) {
-    if (student[1] < 60) {
-        document.write(`${student[0]}: F`);
-    } else if (student[1] < 70) {
-        document.write(`${student[0]}: D`);
-    } else if (student[1] < 80) {
-        document.write(`${student[0]}: C`);
-    } else if (student[1] < 90) {
-        document.write(`${student[0]}: B`);
-    } else if (student[1] < 100) {
-        document.write(`${student[0]}: A`);
-    }
+	if (student[1] < 60) {
+		document.write(`${student[0]}: F`);
+	} else if (student[1] < 70) {
+		document.write(`${student[0]}: D`);
+	} else if (student[1] < 80) {
+		document.write(`${student[0]}: C`);
+	} else if (student[1] < 90) {
+		document.write(`${student[0]}: B`);
+	} else if (student[1] < 100) {
+		document.write(`${student[0]}: A`);
+	}
 
-    document.write("<br>");
+	document.write("<br>");
 }
